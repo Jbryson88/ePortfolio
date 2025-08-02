7c4cf06f7baebe2439bbe4f6bf77cd7741e7b983
John Bryson <john.bryson710@gmail.com>
2025-08-02 16:09:43 -0500

    Delete enhancement/CS 465 - Full Stack Software Development/app_server directory

--- enhancement/CS 465 - Full Stack Software Development/app_server/routes/travel.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const ctrlMain = require('../controllers/travel')
-
-/* GET travel page. */
-router.get('/', ctrlMain.travel)
-
-module.exports = router;
